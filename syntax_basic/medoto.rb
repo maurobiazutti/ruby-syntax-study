952f4b5a1d987581ed34f72b4de509b918ee5f15
Mauro Biazutti <mauro_biazutti@hotmail.com>
2023-11-14 16:23:23 -0300

    Update medoto.rb

--- syntax_basic/medoto.rb
+++ syntax_basic/medoto.rb
@@ -30,18 +30,14 @@ puts "Resultado = #{soma_parametros_nomeados(valor_b: 10, valor_a: 20)}"
 def palindromo1?(palavra)
   palavra = palavra.downcase
   palavra_reversa = ''
-
   indice = palavra.length
-
   until indice.zero?
     letra = palavra[indice - 1]
     palavra_reversa << letra
     indice -= 1
   end
-
   palavra_reversa == palavra
 end
-
 puts palindromo1?('ovo')
 puts palindromo1?('Ovo')
 puts palindromo1?('foo')
